b1fc20c8d61a09a3055add1de2830d5ce0f2f6a4
luminescify <ileahd96@gmail.com>
2021-11-18 19:13:18 -0500

    Started service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,36 @@
+console.log('This is your service-worker.js file!');
+
+const FILES_TO_CACHE = [
+    `/db.js`,
+    `/index.js`,
+    `/index.html`,
+    `/styles.css`,
+    `/manifest.webmanifest`,
+    `/icons/money.png`
+];
+
+const STATIC_CACHE = `static-cache-v1`;
+const RUNTIME_CACHE = `runtime-cache`;
+
+self.addEventListener(`install`, e => {
+    e.waitUntil(
+        caches
+            .open(STATIC_CACHE)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener(`activate`, e => {
+    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+    e.waitUntil(
+        caches
+            .keys()
+            .then(cacheNames => 
+                cacheNames.filter(cacheName => !currentCaches.includes(cacheName))
+            ) .then (deleteCaches => 
+                Promise.all(
+                    deleteCaches.map(deleteCaches => caches.delete(deleteCaches))
+            ))
+    )
+})
