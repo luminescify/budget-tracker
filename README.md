fe9a52f0d0c50bfc9c0a41d096ddf1e6bc2308ee
Leah Davis <ileahd96@gmail.com>
2021-11-18 21:35:15 -0500

    Updated README

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# budget-tracker
+# Budget Tracker
+
+## Description
 Add expenses and deposits to your budget with or without an internet connection.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Mockup](#mockup)
+- [License](#license)
+- [Contribution](#contribution)
+- [Questions](#questions)
+
+## Installation
+Fork this repository and clone it to your local environment. Once cloned, install the necessary dependencies for this project using npm install. This project uses MongoDB so that will also need to be installed on your system.
+
+```
+npm i
+node server.js or npm start
+```
+
+This will launch the server to http://localhost:3000 which you can visit in your browser.
+
+## Usage
+
+This project utilizes JavaScript, Bootstrap, Node, Express, and MongoDB. Functionality includes the ability to add a transaction or deposit, add a description to it, and add transactions and deposits while offline as well.
+
+## Demo
+
+You can find the deployed version of this site [here]().
+
+Upon launch, the tracker will either be blank for a first time user, or will have transactions from a previous use. A basic chart at the top tracks income and expenses and a graph at the bottom of the page tracks a user's income and expenses over time. 
+
+![img](./assets/Budget-Tracker.png)
+
+## License
+
+This application is licensed under MIT.
+
+## Contribution
+
+If you'd like to contribute, please add your code and create a pull request.
+
+---
+
+## Questions?
+For any questions, please see my GitHub or contact me via e-mail. Thank you!
+
+GitHub: [luminescify](https://github.com/luminescify)
+
+E-mail: ileahd96@gmail.com
--- public/js/db.js
+++ public/js/db.js
@@ -4,70 +4,75 @@ let budgetVersion;
 const request = indexedDB.open('BudgetDB', budgetVersion || 21);
 
 request.onupgradeneeded = function (e) {
-    console.log('Upgrade needed in IndexDB');
+  console.log('Upgrade needed in IndexDB');
 
-    const { oldVersion } = e;
-    const newVersion = e.newVersion || db.version;
+  const { oldVersion } = e;
+  const newVersion = e.newVersion || db.version;
 
-    console.log(`DB Updated from version ${oldVersion} to ${newVersion}`);
+  console.log(`DB Updated from version ${oldVersion} to ${newVersion}`);
 
-    db = e.target.result;
+  db = e.target.result;
 
-    if (db.objectStoreNames.length === 0) {
-        db.createdObjectStore('BudgetStore', { autoIncrement: true });
-    }
+  if (db.objectStoreNames.length === 0) {
+    db.createObjectStore('BudgetStore', { autoIncrement: true });
+  }
 };
 
 request.onerror = function (e) {
-    console.log(`Woops! ${e.target.errorCode}`);
-  };
-  
+  console.log(`Woops! ${e.target.errorCode}`);
+};
+
 function checkDatabase() {
-console.log('check db invoked');
+  console.log('check db invoked');
 
-let transaction = db.transaction(['BudgetStore'], 'readwrite');
+  let transaction = db.transaction(['BudgetStore'], 'readwrite');
 
-const store = transaction.objectStore('BudgetStore');
+  const store = transaction.objectStore('BudgetStore');
 
-const getAll = store.getAll();
+  const getAll = store.getAll();
 
-getAll.onsuccess = function () {
+  getAll.onsuccess = function () {
     if (getAll.result.length > 0) {
-    fetch('/api/transaction/bulk', {
+      fetch('/api/transaction/bulk', {
         method: 'POST',
         body: JSON.stringify(getAll.result),
         headers: {
-        Accept: 'application/json, text/plain, */*',
-        'Content-Type': 'application/json',
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
         },
-    })
+      })
         .then((response) => response.json())
         .then((res) => {
-        if (res.length !== 0) {
+          if (res.length !== 0) {
             transaction = db.transaction(['BudgetStore'], 'readwrite');
+
             const currentStore = transaction.objectStore('BudgetStore');
+
             currentStore.clear();
-            }
+            console.log('Clearing store 🧹');
+          }
         });
     }
-};
+  };
 }
 
 request.onsuccess = function (e) {
-console.log('success');
-db = e.target.result;
+  console.log('success');
+  db = e.target.result;
 
-if (navigator.onLine) {
-    console.log('The backend is online');
+  if (navigator.onLine) {
+    console.log('Backend online! 🗄️');
     checkDatabase();
-}
+  }
 };
 
 const saveRecord = (record) => {
-console.log('Save record invoked');
-const transaction = db.transaction(['BudgetStore'], 'readwrite');
-const store = transaction.objectStore('BudgetStore');
-store.add(record);
+  console.log('Save record invoked');
+  const transaction = db.transaction(['BudgetStore'], 'readwrite');
+
+  const store = transaction.objectStore('BudgetStore');
+
+  store.add(record);
 };
 
-window.addEventListener(`online`, checkDatabase);
+window.addEventListener('online', checkDatabase);
